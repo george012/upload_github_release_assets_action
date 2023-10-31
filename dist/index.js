2f1cef25521a37b15508cdbd80086182c44dd39f
WMYeah_Mac <george012@163.com>
2023-10-31 23:33:57 +0800

    Update v1.0.4

--- dist/index.js
+++ dist/index.js
@@ -9619,13 +9619,13 @@ function wrappy (fn, cb) {
 /***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
 
 const core = __nccwpck_require__(2186);
-const { GitHub } = __nccwpck_require__(5438);
+const { getOctokit } = __nccwpck_require__(5438);
 const fs = __nccwpck_require__(7147);
 
 async function run() {
   try {
     // Get authenticated GitHub client (Ocktokit): https://github.com/actions/toolkit/tree/master/packages/github#usage
-    const github = new GitHub(process.env.GITHUB_TOKEN);
+    const github = getOctokit(process.env.GITHUB_TOKEN);
 
     // Get the inputs from the workflow file: https://github.com/actions/toolkit/tree/master/packages/core#inputsoutputs
     const uploadUrl = core.getInput('upload_url', { required: true });
@@ -9634,8 +9634,15 @@ async function run() {
     const assetContentType = core.getInput('asset_content_type', { required: true });
 
     // Determine content-length for header to upload asset
-    const contentLength = (filePath) => fs.statSync(filePath).size;
-
+    // const contentLength = filePath => fs.statSync(filePath).size;
+    const contentLength = filePath => {
+      try {
+        return fs.statSync(filePath).size;
+      } catch (error) {
+        core.setFailed(`Failed to get size of file ${filePath}: ${error.message}`);
+        return 0;
+      }
+    };
     // Setup headers for API call, see Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-upload-release-asset for more information
     const headers = { 'content-type': assetContentType, 'content-length': contentLength(assetPath) };
 
